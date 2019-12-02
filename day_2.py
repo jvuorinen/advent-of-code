143689e5e847cdadecf3fcd6a1141b82318decd1
Jesse Vuorinen <jesse.vuorinen@sok.fi>
2019-12-02 13:22:12 +0200

    added testfile

--- day_2.py
+++ day_2.py
@@ -1,7 +1,34 @@
-def op_1(a: int, b: int):
-    a + b
+import logging
+logging.getLogger().setLevel("INFO")
+
+def add_operation(data, in_a_loc, in_b_loc, res_loc):
+    logging.debug("Doing ADD operation {} {} => {}".format(in_a_loc, in_b_loc, res_loc))
+
+    res = data.copy()
+    res[res_loc] = res[in_a_loc] + res[in_b_loc]
+    return res
+
+def mul_operation(data, in_a_loc, in_b_loc, res_loc):
+    logging.debug("Doing MUL operation {} {} => {}".format(in_a_loc, in_b_loc, res_loc))
+
+    res = data.copy()
+    res[res_loc] = res[in_a_loc] * res[in_b_loc]
+    return res
+
+def parse_command(data, cursor):
 
 
+
+    return operation, args, next_cursor
+
+def compute(data, op_codes):
+    cursor = 0
+    for _ in range(3):
 
 if __name__ == "__main__":
-    
+    op_codes = {
+        1: add_operation
+        2: mul_operation
+    }
+
+    data = [1,9,10,3,2,3,11,0,99,30,40,50]
