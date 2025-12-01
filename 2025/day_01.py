8051bcbe5cacdf97e9d527b6867ee45b00cf3b0b
Vuorinen Jesse <jesse.vuorinen@sok.fi>
2025-12-01 21:13:53 +0200

    cleaner

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -3,21 +3,15 @@ from utils import read, print_answers
 a1 = a2 = 0
 r = 50
 for x in read(2025, 1).split("\n"):
-    a = -1 if x[0] == "L" else 1
-    b = int(x[1:])
-    n = (a * b)
-
+    n = int(x[1:]) * (-1, 1)[x[0] == "R"]
     d, m = divmod(r + n, 100)
     a1 += (m == 0)
-
     if n > 0:
         a2 += d
-    elif r > 0:
-        a2 += abs(d) + (m == 0)
     else:
-        a2 += abs(d) - 1 + (m == 0)
-
+        a2 += abs(d) + (m == 0)
+        if r == 0:
+            a2 -= 1
     r = m
-
 
 print_answers(a1, a2, day=1)
