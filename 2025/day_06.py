9390c1baca3262496152ac64479cb23e8c76849e
Vuorinen Jesse <jesse.vuorinen@sok.fi>
2025-12-06 09:28:53 +0200

    cleaner

--- 2025/day_06.py
+++ 2025/day_06.py
@@ -1,13 +1,15 @@
 from itertools import groupby
 from utils import read, print_answers
 
-raw = read(2025, 6).split("\n")
+*nums, ops = read(2025, 6).split("\n")
+ops = ops.split()
 
-a1 = sum(eval(op.join(ns)) for *ns, op in zip(*map(str.split, raw)))
+g1 = list(zip(*map(str.split, nums)))
 
-tmp = [n if not (n := "".join(x)).isspace() else None for x in zip(*raw[:-1])]
-groups = [list(g) for k, g in groupby(tmp, lambda x: x is None) if not k]
-ops = raw[-1].split()
-a2 = sum(eval(op.join(g)) for g, op in zip(groups, ops))
+joined = ["".join(x).strip() for x in zip(*nums)]
+g2 = [list(g) for k, g in groupby(joined, lambda x: x != "") if k]
+
+a1 = sum(eval(op.join(g)) for g, op in zip(g1, ops))
+a2 = sum(eval(op.join(g)) for g, op in zip(g2, ops))
 
 print_answers(a1, a2, day=6)
