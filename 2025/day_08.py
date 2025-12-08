8913e468b0a5d347d9eba93fd28c689aef8bb453
Vuorinen Jesse <jesse.vuorinen@sok.fi>
2025-12-08 09:00:34 +0200

    tuning

--- 2025/day_08.py
+++ 2025/day_08.py
@@ -3,25 +3,16 @@ from math import prod
 from utils import read, print_answers
 
 raw = read(2025, 8).split("\n")
+boxes = [tuple(map(int, x.split(","))) for x in raw]
+pairs = sorted(combinations(boxes, 2), key=lambda pair: sum([(a - b) ** 2 for a, b in zip(*pair)]))
 
-boxes = [tuple(map(int, x.split(','))) for x in raw]
-pairs = sorted(
-    combinations(boxes, 2),
-    key=lambda pair: sum([(a-b)**2 for a, b in zip(*pair)])
-)
-circuits = [{x} for x in boxes]
-
-a2 = None
+circuits = {frozenset({x}) for x in boxes}
 for i, (a, b) in enumerate(pairs, 1):
-    join = []
-    for c in circuits:
-        if (a in c) ^ (b in c):
-            join.append(c)
-    if len(join) == 2:
-        circuits.remove(join[0])
-        circuits.remove(join[1])
-        circuits.append(set.union(*join))
-    if not a2 and len(circuits) == 1:
+    join = {c for c in circuits if (a in c) ^ (b in c)}
+    if join:
+        circuits -= join
+        circuits.add(frozenset.union(*join))
+    if len(circuits) == 1:
         a2 = a[0] * b[0]
         break
     if i == 1000:
