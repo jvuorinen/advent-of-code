14400abb4bee6a5f3e74005ac3e7492e418a97da
Vuorinen Jesse <jesse.vuorinen@sok.fi>
2025-12-08 08:50:58 +0200

    bugfix

--- 2025/day_08.py
+++ 2025/day_08.py
@@ -3,7 +3,6 @@ from math import prod
 from utils import read, print_answers
 
 raw = read(2025, 8).split("\n")
-# raw = read().split("\n")
 
 boxes = [tuple(map(int, x.split(','))) for x in raw]
 pairs = sorted(
@@ -13,22 +12,18 @@ pairs = sorted(
 circuits = [{x} for x in boxes]
 
 a2 = None
-for i, (a, b) in enumerate(pairs):
+for i, (a, b) in enumerate(pairs, 1):
     join = []
     for c in circuits:
         if (a in c) ^ (b in c):
-            circuits.remove(c)
             join.append(c)
-    if join:
-        # if len(join) != 2:
-        #     print(len(join))
+    if len(join) == 2:
+        circuits.remove(join[0])
+        circuits.remove(join[1])
         circuits.append(set.union(*join))
-    if not a2 and len(circuits) == 2:
+    if not a2 and len(circuits) == 1:
         a2 = a[0] * b[0]
         break
-    # print(a, b)
-    # print(circuits)
-    # print(i, list(map(len, circuits)))
     if i == 1000:
         a1 = prod(sorted(map(len, circuits))[-3:])
 
