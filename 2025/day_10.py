1295ee4fa096adf16da421d91fc44a1d40d50988
Vuorinen Jesse <jesse.vuorinen@sok.fi>
2025-12-10 19:08:06 +0200

    d10 works

---
+++ 2025/day_10.py
@@ -0,0 +1,62 @@
+import numpy as np
+from re import findall
+
+from ortools.sat.python import cp_model
+from utils import read, print_answers
+
+raw = read(2025, 10).split("\n")
+
+machines = []
+for line in raw:
+    a, b, c = findall(r"\[(.*)\] (.*)\{(.*)}", line)[0]
+    target = tuple([x == "#" for x in a])
+    buttons = [tuple(map(int, x.split(","))) for x in findall(r"\(([,\d]*)\)", b)]
+    jolts = tuple(map(int, c.split(",")))
+    machines.append((target, buttons, jolts))
+
+
+def bfs(machine):
+    target, buttons, _ = machine
+
+    start = tuple([False] * len(target))
+    batch = [start]
+    for i in range(1000):
+        _batch = []
+        for state in batch:
+            if state == target:
+                return i
+            for b in buttons:
+                _state = list(state)
+                for ix in b:
+                    _state[ix] = not _state[ix]
+                _batch.append(tuple(_state))
+        batch = _batch
+
+
+def solve_ilp(machine):
+    _, buttons, jolts = machine
+
+    model = cp_model.CpModel()
+    x = [model.NewIntVar(0, 1000, f"x{i}") for i in range(len(buttons))]
+
+    coeffs = np.array([[0]*len(jolts)]*len(buttons))
+    for i, b in enumerate(buttons):
+        for j in b:
+            coeffs[i, j] = 1
+    coeffs = coeffs.T
+
+    for cf, jolt in zip(coeffs, jolts):
+        ixs = np.where(cf)[0]
+        model.Add(sum(x[i] for i in ixs) == jolt)
+
+    model.Minimize(sum(x))
+
+    solver = cp_model.CpSolver()
+    solver.Solve(model)
+    return sum(solver.Value(v) for v in x)
+
+
+a1 = sum(map(bfs, machines))
+a2 = sum(map(solve_ilp, machines))
+
+print_answers(a1, a2, day=10)
