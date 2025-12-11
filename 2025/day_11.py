bbaf7b1de0f8ff8c4e5cce8aaab46970ebf61430
Vuorinen Jesse <jesse.vuorinen@sok.fi>
2025-12-11 07:24:13 +0200

    p1

--- 2025/day_11.py
+++ 2025/day_11.py
@@ -8,11 +8,29 @@ import networkx as nx
 from tqdm import tqdm
 from utils import read, print_answers
 
-raw = read().split("\n")
-# raw = read(2025, None).split("\n")
+# raw = read().split("\n")
+raw = read(2025, 11).split("\n")
+
+G = {}
+for line in raw:
+    a, b = line.split(': ')
+    G[a] = b.split(' ')
 
 
-a1 = None
-a2 = None
+def crawl(node, end, cnt = 0, path = None):
+    path = path or []
+    if node == end:
+        return 1
+    return sum( crawl(n, end, cnt, path) for n in G.get(node, []))
 
-print_answers(a1, a2, day=99)
+crawl("you", "out")
+
+crawl("dac", "out")
+crawl("fft", "out")
+
+
+
+a1 = len(tally["paths"])
+a2 = len([x for x in tally["paths"] if "dac" in ])
+
+print_answers(a1, a2, day=11)
