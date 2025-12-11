e9ecdfd6413aa7ef685635a04d74ebec8e88d111
Vuorinen Jesse <jesse.vuorinen@sok.fi>
2025-12-11 20:17:49 +0200

    stuff

--- 2025/day_10_fiddling.py
+++ 2025/day_10_fiddling.py
@@ -4,12 +4,13 @@ from utils import read, print_answers
 
 raw = read(2025, 10).split("\n")
 
+
 machines = []
 for line in raw:
     a, b, c = findall(r"\[(.*)\] (.*)\{(.*)}", line)[0]
     target = tuple([x == "#" for x in a])
     buttons = [tuple(map(int, x.split(","))) for x in findall(r"\(([,\d]*)\)", b)]
-    buttons = sorted(buttons, key=len)[::-1]
+    buttons = sorted(buttons, key=lambda x: (len(x), x))[::-1]
     jolts = tuple(map(int, c.split(",")))
     machines.append((target, buttons, jolts))
 
@@ -32,29 +33,68 @@ def bfs(machine):
         batch = _batch
 
 tally = {
-    "best": 999
+    "best": 300
 }
-def dfs(buttons, jolts, ix=0, cnt=0):
-    if len(buttons) == 0:
-        if cnt < tally["best"]:
-            tally["best"] = cnt
-        return cnt
+
+
+def build_lookups(buttons):
+    everybody = set.union(*[set(b) for b in buttons])
+    nb = len(buttons)
+
+    cant_avoid = []
+    for i in range(nb):
+        d = {}
+        for e in everybody:
+            relevant = [x for x in buttons[i:] if e in x]
+            omnipresent = {ee for ee in everybody if all([ee in x for x in relevant])} - {e}
+            if omnipresent:
+                d[e] = omnipresent
+        cant_avoid.append(d)
+    
+    available = []
+    for i in range(nb):
+        available.append(set.union(*[set(x) for x in buttons[i:]]))
+
+    return cant_avoid, available
+
+def dfs(buttons, jolts, ix=0, cnt=0, cant_avoid=None, available=None):
+    if cant_avoid is None:
+        cant_avoid, available = build_lookups(buttons)
+    # print(jolts, ix, cnt, br)
+    if (len(buttons) == 0):
+        if sum(jolts) == 0:
+            if cnt < tally["best"]:
+                print(f"Best {cnt}")
+                tally["best"] = cnt
+            return cnt
+        return 999
     if any([x < 0 for x in jolts]):
         return 999
-    if not any([ix in b for b in buttons]):
+
+    needed = set([i for i, x in enumerate(jolts) if x > 0])
+    if not ((needed & available[ix]) == needed):
         return 999
+
+    dontpush = set([i for i, x in enumerate(jolts) if x == 0])
+    for x in needed:
+        if dontpush & cant_avoid[ix].get(x, set()):
+            return 999
+
     _pressed = jolts[:]
     for b in buttons[0]:
         _pressed[b] -= 1
+
     return min(
-        dfs(buttons, _pressed, ix, cnt+1),
-        dfs(buttons[1:], _pressed, ix+1, cnt+1),
-        dfs(buttons[1:], jolts, ix+1, cnt),
+        dfs(buttons[:], _pressed, ix, cnt+1, cant_avoid, available),
+        dfs(buttons[1:], _pressed, ix+1, cnt+1, cant_avoid, available),
+        dfs(buttons[1:], jolts, ix+1, cnt, cant_avoid, available),
     )
 
+machine = machines[1]
 _, buttons, jolts = machine
+solve_ilp(machine)
 
-dfs(buttons, list(jolts))
+%time dfs(buttons, list(jolts))
 
 def solve_ilp(machine):
     _, buttons, jolts = machine
